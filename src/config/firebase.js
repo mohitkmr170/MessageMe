58412a5f3280ee61c04844f1e5f0afe1a307c7a2
Mohit Kumar <mohitk@geekyants.com>
2019-09-06 12:58:48 +0530

    login page and firebase integration for phone Auth

---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyASQ9p6RIGxRiWOcno1LcWGTlSrOmzNLsM',
+  authDomain: 'messageme-59576.firebaseapp.com',
+  databaseURL: 'https://messageme-59576.firebaseio.com',
+  projectId: 'messageme-59576',
+  storageBucket: '',
+  messagingSenderId: '981978226078',
+  appId: '1:981978226078:ios:c6dc5a27e05635b3',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
